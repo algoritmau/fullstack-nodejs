12adb8bf82c5c5f251c0e8eebee4444e15571e3a
Mauricio Paternina <spaceinvadev@gmail.com>
2021-08-19 10:28:16 -0500

    add basic routing support

--- 01-first-node-api/01-server.js
+++ 01-first-node-api/01-server.js
@@ -1,10 +1,35 @@
 const http = require('http')
+
 const port = process.env.PORT || 1337
-const server = http.createServer((_, res) => {
-  // res.end('Hello World!')
+
+function respondText(_, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello World\n')
+}
+
+function respondJson(_, res) {
   res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({ message: 'Hello World!', numbers: [1, 2, 3] }))
+  res.end(JSON.stringify({ message: 'Hello World' }))
+}
+
+function respond404(_, res) {
+  res.statusCode = 404
+  res.end('Not found')
+}
+
+const server = http.createServer((req, res) => {
+  switch (req.url) {
+    case '/':
+      return respondText(req, res)
+
+    case '/json':
+      return respondJson(req, res)
+
+    default:
+      return respond404(req, res)
+  }
 })
 
 server.listen(port)
+
 console.log(`Server listening on port ${port}`)
