71194b737f862efb154db2c9c529a445454d0df6
Mauricio Paternina <spaceinvadev@gmail.com>
2021-08-19 10:13:00 -0500

    add functionality to serve json response

--- 01-first-node-api/01-server.js
+++ 01-first-node-api/01-server.js
@@ -1,7 +1,9 @@
 const http = require('http')
 const port = process.env.PORT || 1337
 const server = http.createServer((_, res) => {
-  res.end('Hello World!')
+  // res.end('Hello World!')
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify({ message: 'Hello World!', numbers: [1, 2, 3] }))
 })
 
 server.listen(port)
